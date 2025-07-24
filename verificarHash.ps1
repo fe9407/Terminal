f927324976241ccf4c0ca653a9a19c9271016068
Fe_9407ツ <117548660+fe9407@users.noreply.github.com>
2025-07-23 22:30:06 -0300

    Add files via upload

--- verificarHash.ps1
+++ verificarHash.ps1
@@ -1,13 +1,13 @@
 write-host "Digite o caminho do arquivo (sem aspas)" -ForegroundColor Yellow
 $caminho = read-host
 
-write-host "Digite o hash do arquivo" -ForegroundColor Yellow
+write-host "Digite o hash esperado do arquivo" -ForegroundColor Yellow
 $hashesperado = read-host
 
 $hashcalculado = (Get-FileHash -Path $caminho -Algorithm SHA256).Hash
 
 if ($hashcalculado -eq $hashesperado) {
-    Write-Host "arquivo oficial" -ForegroundColor Green
+    write-host "arquivo oficial" -ForegroundColor Green
 } else {
-    Write-Host "arquivo não oficial" -ForegroundColor Red
+    write-host "arquivo não oficial" -ForegroundColor Red
 }
